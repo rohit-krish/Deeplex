2792863fde96b161667fb012e150d9924ac7ce3c
Rohit <dev.rohitnp@gmail.com>
2023-08-20 17:29:20 +0530

    updates to encorporate the .requires_grad

--- deeplex/nn/_act_func.py
+++ deeplex/nn/_act_func.py
@@ -2,40 +2,52 @@ from ..engine import Tensor
 
 
 def relu(val: Tensor):
-    res = Tensor(val.d.maximum(0, val.data), (val,), "ReLU", val.device)
+    res = Tensor(val.d.maximum(0, val.data), val.device, val.dtype, (val,))
 
-    def backward():
-        val.grad += (val.data > 0) * res.grad
+    if val.requires_grad and Tensor.grad_enabled:
 
-    res._backward = backward
+        def backward():
+            val.grad += (val.data > 0) * res.grad
+
+        res._backward = backward
+        res.requires_grad = True
+
     return res
 
 
 def tanh(val: Tensor):
-    res = Tensor(val.d.tanh(val.data), (val,), "TanH", val.device)
+    res = Tensor(val.d.tanh(val.data), val.device, val.dtype, (val,))
 
-    def backward():
-        val.grad += (1 - res.data**2) * res.grad
+    if val.requires_grad and Tensor.grad_enabled:
 
-    res._backward = backward
+        def backward():
+            val.grad += (1 - res.data**2) * res.grad
+
+        res._backward = backward
+        res.requires_grad = True
+
     return res
 
 
 def sigmoid(val: Tensor):
     exp_x = val.d.exp(-val.data)
-    res = Tensor(1 / (1 + exp_x), (val,), "Sigmoid", val.device)
+    res = Tensor(1 / (1 + exp_x), val.device, val.dtype, (val,))
 
-    def backward():
-        val.grad += (exp_x / (1 + exp_x) ** 2) * res.grad
+    if val.requires_grad and Tensor.grad_enabled:
 
-    res._backward = backward
+        def backward():
+            val.grad += (exp_x / (1 + exp_x) ** 2) * res.grad
+
+        res._backward = backward
+        res.requires_grad = True
+
     return res
 
 
 # the below softmax implementation is taking advantage of the autograd engine.
 def softmax(val: Tensor, axis=-1):
     # applying "log-sum-exp"(subtracting the max from the input) trick to avoid numerical instability when taking exp
-    max_val = Tensor(val.data.max(axis=axis, keepdims=True), device=val.device)
+    max_val = Tensor(val.data.max(axis=axis, keepdims=True), val.device, val.dtype)
     exp_shifted_x = (val - max_val).exp()
 
     sum_exp_shifted_x = exp_shifted_x.sum(axis=axis, keepdims=True)
